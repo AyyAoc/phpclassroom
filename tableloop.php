d7c9d7a846a135d570357b50cb5bc7aa41d4df95
AyyAoc <89377481+AyyAoc@users.noreply.github.com>
2021-11-26 00:30:47 +0700

    Update tableloop.php

--- tableloop.php
+++ tableloop.php
@@ -4,9 +4,9 @@
   <body>
     <table border="1">
      <?php
-       for($i=0;$i<6;$i++){
+       for($i=0;$i<100;$i++){
          echo "<tr>";
-         for($j=0;$j<100;$j++){
+         for($j=0;$j<6;$j++){
           echo "<td>คอลัมน์</td>";}
          echo "</tr>";
        }
